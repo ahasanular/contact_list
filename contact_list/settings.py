571e0b347f6dcaa6dbaea87998a5bee4fba721c6
jahid11186 <zahidhasansamim@gmail.com>
2022-03-14 01:24:02 +0600

    db commit

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'contact_manager',
         'USER': 'root',
-        'PASSWORD': 'Hasani*1',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
