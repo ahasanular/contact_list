5b4d364a7667c9b1d39797f575393848f8337e4f
AhasanulWorkstation <ahasanular@gmail.com>
2022-03-13 21:52:51 +0600

    Api and Frontend created for contact lists

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.contactList),
+    # path('details', views.),
 
+    # api
+    path('contact_list_api/', views.contact_list_api.as_view()),
+    # path('contact_edit_api/', views.contact_edit_api.as_view()),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Person
+from .serializers import PersonSerializer
 
-# Create your views here.
+def contactList(request):
+    return render(request, 'index.html')
+
+
+#api
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
+
+class contact_list_api(ListAPIView):
+    permission_classes = []
+    def get(self, request):
+        data = Person.objects.filter().all()
+        data = PersonSerializer(data, many=True).data
+        return Response(data)
+
+class contact_list_api(CreateAPIView):
+    permission_classes = []
+    def get(self, request):
+        data = Person.objects.filter().all()
+        data = PersonSerializer(data, many=True).data
+        return Response(data)
