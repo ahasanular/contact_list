9d8dce5262e884214db4eb7f42cb44b178b7cade
jahid11186 <zahidhasansamim@gmail.com>
2022-03-13 22:55:32 +0600

    Sign up api added in to urls.py

--- registration/urls.py
+++ registration/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserSignIn
+from .views import UserSignUp, UserSignIn
 
 urlpatterns = [
     # API urls
-    path('api/sign_in', UserSignIn.as_view()),
+    path('api/sign_up/', UserSignUp.as_view()),
+    path('api/sign_in/', UserSignIn.as_view()),
 ]
