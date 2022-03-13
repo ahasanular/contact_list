2792235404cb7fe4cd9edab2e23f1816d9011800
Mynul Hasan <mdhasan008998@gmail.com>
2022-03-13 21:05:22 +0600

    my sql connect done

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -87,9 +87,9 @@ WSGI_APPLICATION = 'contact_list.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'school',
+        'NAME': 'contact_manager',
         'USER': 'root',
-        'PASSWORD': 'Ar@fath21',
+        'PASSWORD': 'mynameis00',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
