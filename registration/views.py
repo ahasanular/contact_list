e40f2893be5c379cf718f74dde94bc1b47359aff
jahid11186 <zahidhasansamim@gmail.com>
2022-03-13 22:39:34 +0600

    sign up API added

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+class UserSignUp(CreateAPIView):
+    permission_classes = []
 
-# Create your views here.
+    def post(self, request):
+        result = {}
+        try:
+            data = request.data
+
+            if 'full_name' not in data or data['full_name'] == '':
+                return Response("Full name can not be null.")
+            if 'email' not in data or data['email'] == '':
+                return Response("Email can not be null.")
+            if 'password' not in data or data['password'] == '':
+                return Response("Password can not be null.")
+
+            user = User.objects.filter(email=data['email']).first()
+
+            if user:
+                return Response("You have account")
+
+            if not user:
+                user = User()
+                user.username = data['full_name']
+                user.first_name = data['full_name']
+                user.email = data['email']
+                user.password = make_password(data['password'])
+                user.save()
+                app_user = AppUser()
+                app_user.user = user
+                app_user.full_name = data['full_name']
+                app_user.email = data['email']
+                app_user.save()
