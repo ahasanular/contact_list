2f726de92a62075be4cfe36ba8d36b36897b2bfd
AhasanulWorkstation <ahasanular@gmail.com>
2022-03-14 00:00:13 +0600

    edit api

--- contacts/urls.py
+++ contacts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 
     # api
     path('contact_list_api/', views.contact_list_api.as_view()),
-    # path('contact_edit_api/', views.contact_edit_api.as_view()),
+    path('contact_edit_api/<slug:slug>/', views.contact_edit_api.as_view()),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Person
 from .serializers import PersonSerializer
+import json
 
 def contactList(request):
     return render(request, 'index.html')
@@ -23,3 +24,20 @@ class contact_list_api(CreateAPIView):
         data = Person.objects.filter().all()
         data = PersonSerializer(data, many=True).data
         return Response(data)
+
+class contact_edit_api(CreateAPIView):
+    permission_classes = []
+    def put(self, request, slug):
+        try:
+            data = json.loads(request.body)
+
+            person = Person.objects.filter(slug=slug).first()
+
+            if not person:
+                feedback = {}
+                feedback['status'] = HTTP_400_BAD_REQUEST
+                feedback['message'] = "Person was not found !"
+                return Response(feedback)
+            else:
+                person.email = data['email']
+                person.name =
