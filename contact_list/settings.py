2574a37f6294f833f791af6ca6d057034c620928
jahid11186 <zahidhasansamim@gmail.com>
2022-03-16 16:51:42 +0600

    final check done

--- contact_list/settings.py
+++ contact_list/settings.py
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'contact_list.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'con_man',
+        'NAME': 'contact_manager',
         'USER': 'root',
-        'PASSWORD': 'Hasani*1',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
