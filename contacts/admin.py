42a71528e6f6cdbe88d2646b7aa0ef6624cd9f67
AhasanulWorkstation <ahasanular@gmail.com>
2022-06-19 04:17:19 +0600

    modified admin view a bit

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,8 +3,13 @@ from .models import Person, DeletedContacts
 
 
 class PersonAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        queryset = Person.objects.filter(is_archived__in=[False]).all().order_by('id')
+        return queryset
+
     fields = ['user', 'name', 'email', 'phone1', 'phone2', 'phone3', 'is_archived', 'slug', 'qr_code']
     list_display = ['name', 'email', 'phone1', 'phone2', 'phone3', 'slug', 'is_archived', 'qr_code']
+    list_per_page = 30
     readonly_fields = ['slug', 'qr_code']
     search_fields = ['name__icontains']
 
